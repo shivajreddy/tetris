547bf229ac88a803db4f9020d26c3116502aca7a
smpl <sreddy@hey.com>
2025-10-22 19:46:23 -0400

    added gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
-#include <iostream>
 #include "raylib.h"
+#include <iostream>
 
 using namespace std;
 
+int main() {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 400;
 
-int main(){
-  const int SCREEN_WIDTH = 800;
-  const int SCREEN_HEIGHT = 400;
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tetris");
+    SetTargetFPS(60);
 
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tetris");
-  SetTargetFPS(60);
-
-  // Main game loop
+    // Main game loop
     while (!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground(DARKBLUE);
+        ClearBackground(DARKPURPLE);
         EndDrawing();
     }
-    
+
     // Cleanup
     CloseWindow();
 
-  return 0;
+    return 0;
 }
